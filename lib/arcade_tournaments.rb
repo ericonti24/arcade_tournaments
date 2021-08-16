e0b3fb65be16d42b617955066849a6f9e975d6af
Eric <ericontiveros123@gmail.com>
2021-08-16 11:54:32 -0700

    add desc

---
+++ lib/arcade_tournaments.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "arcade_tournaments/version"
+
+module ArcadeTournaments
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/arcade_tournaments/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ArcadeTournaments
+  VERSION = "0.1.0"
+end
